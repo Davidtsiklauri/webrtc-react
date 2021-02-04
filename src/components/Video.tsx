e849c0ba14ccae28ce4fa8303ec82bc0e504512e
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-02-04 21:33:46 +0400

    js to typescript, add prettier config

---
+++ src/components/Video.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Video = () => {
+  return (
+    <video width="750" height="500" controls>
+      <source src="./Videos/video1.mp4" type="video/mp4" />
+    </video>
+  );
+};
