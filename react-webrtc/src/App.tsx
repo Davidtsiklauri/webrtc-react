4e0dcc3ecbb6b084c81924880cdb6931192de7a7
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-02-05 12:07:56 +0400

    added server

---
+++ react-webrtc/src/App.tsx
@@ -0,0 +1,48 @@
+import React, { createRef } from 'react';
+import { Video } from './components/Video';
+import { userMedia } from './utils/mediaHelper';
+
+function App() {
+  const videoRef = createRef() as React.RefObject<HTMLVideoElement>;
+  (async () => {
+    const stream: MediaStream = await userMedia.getUserMedia();
+    if (videoRef.current) {
+      const { current } = videoRef;
+      current.srcObject = stream;
+      current.play();
+    }
+  })();
+
+  // const signalingChannel = new SignalingChannel(remoteClientId);
+  // signalingChannel.addEventListener('message', message => {
+  // });
+
+  // signalingChannel.send('Hello!');
+
+  const makeCall = async () => {
+    console.log('in Make Call');
+
+    const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
+    const peerConnection = new RTCPeerConnection(configuration);
+    // signalingChannel.addEventListener('message', async message => {
+    //     if (message.answer) {
+    //         const remoteDesc = new RTCSessionDescription(message.answer);
+    //         await peerConnection.setRemoteDescription(remoteDesc);
+    //     }
+    // });
+    const offer = await peerConnection.createOffer();
+    console.log(offer);
+
+    await peerConnection.setLocalDescription(offer);
+    // signalingChannel.send({'offer': offer});
+  };
+
+  return (
+    <>
+      <Video ref={videoRef} />
+      <button onClick={makeCall}>Make A Call</button>
+    </>
+  );
+}
+
+export default App;
