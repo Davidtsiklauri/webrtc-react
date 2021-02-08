d66de7596648fa9dbb0f295c5e6b0a90bd9213c7
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-02-08 11:43:33 +0400

    added close call func,need to fix bug

--- react-webrtc/src/App.tsx
+++ react-webrtc/src/App.tsx
@@ -24,22 +24,36 @@ const rtcpHelper = new RtcpHelper(CONFIG),
 function App() {
   const [isVisible, setVisibility] = useState(false);
   const [offer, setOffer] = useState({});
+  const [isCallHangup, setCallHangup] = useState(true);
 
   useEffect(() => {
     rtcpHelper.addEventListener('icecandidate', (ev: RTCPeerConnectionIceEvent) => {
       console.log(ev);
+      // try {
+      //   if (ev.candidate) {
+      //     rtcpHelper.peerConnection.addIceCandidate(ev.candidate);
+      //   }
+      // } catch (e) {}
     });
 
     rtcpHelper.addEventListener('connectionstatechange', (ev: RTCPeerConnectionIceEvent) => {
       // console.log(ev);
+      setCallHangup(false);
+    });
+
+    rtcpHelper.addEventListener('connectionstatechange', (ev: RTCPeerConnectionIceEvent) => {
+      console.log('PEERS CONNECTEd');
     });
 
     socket.messageListener<EVENT>(async ({ offer }: { offer: RTCSessionDescriptionInit }) => {
+      console.log('offer');
+
       setVisibility(true);
       setOffer(offer);
     }, 'offer');
 
     socket.messageListener<EVENT>(({ answer }: { answer: RTCSessionDescriptionInit }) => {
+      console.log('offer');
       rtcpHelper.setDescription(answer);
     }, 'answer');
 
@@ -47,7 +61,6 @@ function App() {
       if (videoRef.current) {
         const { current } = videoRef;
         current.srcObject = stream;
-        console.log(stream.getTracks());
       }
     };
 
@@ -70,6 +83,11 @@ function App() {
     socket.emit<EVENT>('offer', offer);
   };
 
+  const closeCall = () => {
+    rtcpHelper.peerConnection.close();
+    setCallHangup(true);
+  };
+
   const onModalClose = async (isCancel: boolean = false) => {
     if (!isCancel) {
       await rtcpHelper.setDescription(offer);
@@ -86,9 +104,16 @@ function App() {
         <Video ref={callVideoRef} />
         <Video ref={videoRef} />
       </div>
-      <Button onClick={makeCall} type="primary">
-        Make A Call
-      </Button>
+      {(isCallHangup && (
+        <Button onClick={makeCall} type="primary">
+          Make A Call
+        </Button>
+      )) || (
+        <Button onClick={closeCall} type="primary">
+          Close Call
+        </Button>
+      )}
+
       <ModalWrapper
         isVisible={isVisible}
         onConfrim={() => onModalClose(false)}
