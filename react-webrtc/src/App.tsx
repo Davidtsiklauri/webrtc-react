42036de7feb1de51bb8d17fd814072e538ab1292
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-02-11 03:53:17 +0400

    need to fix redux

--- react-webrtc/src/App.tsx
+++ react-webrtc/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { UserCallWrapper } from './components/UserCallWrapper';
+import { UserCallWrapper } from './features/call/UserCallWrapper';
 
 function App() {
   return <UserCallWrapper />;
--- react-webrtc/src/index.tsx
+++ react-webrtc/src/index.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  document.getElementById('root'),
+);
